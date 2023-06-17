6b9fe3bd0c9ede6a3ec348b02eefb47ea863ca8a
mahyarnafisi <mahyar.nafisi@gmail.com>
2023-06-17 20:23:15 +0200

    ..

--- src/components/user/AddUser.js
+++ src/components/user/AddUser.js
@@ -1,7 +1,22 @@
 import React from "react";
 import styles from "../style/user/AddUser.module.css";
+import { BsPlusCircle } from "react-icons/bs";
 function AddUser() {
-  return <div className={styles["add-user"]}></div>;
+  return (
+    <form className={styles["add-user"]}>
+      <label className={styles["add-user__label"]} htmlFor="username">
+        Username
+      </label>
+      <input className={styles["add-user__input"]} id="username" type="text" placeholder="enter your name" />
+      <label className={styles["add-user__label"]} htmlFor="username">
+        Your Age
+      </label>
+      <input className={`${styles["add-user__input"]} `} id="username" type="number" placeholder="enter your age" />
+      <button className={styles["add-user__btn-submit"]}>
+        <BsPlusCircle />
+      </button>
+    </form>
+  );
 }
 
 export default AddUser;
