87430b29c92b723e358658d7b9ef8101c8f2e65a
mahyarnafisi <mahyar.nafisi@gmail.com>
2023-06-17 19:51:20 +0200

    new start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import Styles from "./App.module.css";
+import AddUser from "./components/user/AddUser";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={Styles.app}>
+      <AddUser />
     </div>
   );
 }
---
+++ src/components/user/AddUser.js
@@ -0,0 +1,7 @@
+import React from "react";
+import styles from "../style/user/AddUser.module.css";
+function AddUser() {
+  return <div className={styles["add-user"]}></div>;
+}
+
+export default AddUser;
